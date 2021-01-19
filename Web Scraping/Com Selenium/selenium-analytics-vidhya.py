cc131489677d04ca004ce3eb340c7993191daafc
lucas <skylucacius@hotmail.com>
2021-01-19 20:48:21 -0300

    tutorial webscraping analytics vidhya iniciado

---
+++ Web Scraping/Com Selenium/selenium-analytics-vidhya.py
@@ -0,0 +1,13 @@
+import os, io, requests, selenium, time
+from selenium import webdriver
+# from PIL import Image
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
+
+search_url= 'https://www.google.com/search?q={q}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568'
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(search_url.format(q='Car'))
+#Scroll to the end of the page
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(5)#sleep_between_interactions
+
