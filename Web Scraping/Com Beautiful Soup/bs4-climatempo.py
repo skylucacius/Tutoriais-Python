b263daf83f77ff1acb77c55d3ba6af3d7d624c72
lucas <skylucacius@hotmail.com>
2021-01-24 07:20:31 -0300

    pequeno ajuste na string de saída

--- Web Scraping/Com Beautiful Soup/bs4-climatempo.py
+++ Web Scraping/Com Beautiful Soup/bs4-climatempo.py
@@ -4,10 +4,10 @@ html = requests.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/558/
 soup = BeautifulSoup(html, 'html.parser')
 # print(soup.prettify())
 try:
-    temperatura_minima = soup.find("span", id="min-temp-1")
-    temperatura_maxima = soup.find("span", id="max-temp-1")
-    print("a temperatura máxima é: " + temperatura_maxima)
-    print("a temperatura mínima é: " + temperatura_minima)
+    temperatura_minima = soup.find("span", id="min-temp-1").string
+    temperatura_maxima = soup.find("span", id="max-temp-1").string
+    print("a temperatura máxima é: " + temperatura_maxima + "C")
+    print("a temperatura mínima é: " + temperatura_minima + "C")
 except Exception as e:
     print(e)
     
