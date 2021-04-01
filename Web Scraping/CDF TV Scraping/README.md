3a5ec5cecc63133e66d27fa91dcfa788fb22941e
skylucacius <skylucacius@hotmail.com>
2021-03-31 21:30:42 -0300

    primeiro dia no tutorial

---
+++ Web Scraping/CDF TV Scraping/README.md
@@ -0,0 +1,12 @@
+# Web Scraping com Python usando Selenium e Pandas
+
+Instalar as dependências:
+> webdriver-manager.
+> beautifulsoup4;
+> pandas;
+> selenium.
+
+Num ambiente virtual de preferência, mas nada impede de instalar na própria máquina via pip install. Em seguida basta executar o código.
+
+Baseado no tutorial disponível em: 
+https://www.youtube.com/watch?v=Vxl5jUltHBo
--- Web Scraping/CDF TV Scraping/Web Scraping.py
+++ Web Scraping/CDF TV Scraping/Web Scraping.py
@@ -2,12 +2,14 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import ChromeOptions
 import json
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 
 # Grab content from URL (Pegar conteúdo HTML a partir da URL)
-url = "https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
+url = "https://www.nba.com/stats/players/traditional/?sort=W&dir=-1"
 top10ranking = {}
 
 # rankings = {
@@ -44,20 +46,61 @@ top10ranking = {}
 #     # Convert to Dict (Transformar os Dados em um Dicionário de dados próprio)
 #     return df.to_dict('records')
 
+options = webdriver.ChromeOptions()
+options.headless = False
+driver = webdriver.Chrome(ChromeDriverManager().install() , options=options)
+from time import sleep
+driver.get(url)
 
-# option = Options()
-# option.headless = True
-# driver = webdriver.Firefox(options=option)
+driver.implicitly_wait(15)  # in seconds
+# sleep(60)
 
-# driver.get(url)
-# driver.implicitly_wait(10)  # in seconds
+seletor_xpath = lambda campo : f"//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='{campo}']"
+pagina_html = "//div[@class='nba-stat-table']//table"
+
+try:
+    # driver.find_element_by_xpath(seletor_xpath("PTS")).click()
+    elemento = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
+    tabela = elemento.get_attribute("outerHTML")
+except NoSuchElementException as erro:
+    print("elemento não encontrado: " + erro.msg)
+except ElementClickInterceptedException as erro:
+    print("Elemento foi interceptado por outro elemento: \n\n" + erro.msg)
+
+driver.implicitly_wait(15)  # in seconds
+# sleep(60)
+
+
 
 # for k in rankings:
 #     top10ranking[k] = buildrank(k)
 
-# driver.quit()
+driver.quit()
 
 # # Dump and Save to JSON file (Converter e salvar em um arquivo JSON)
 # with open('ranking.json', 'w', encoding='utf-8') as jp:
 #     js = json.dumps(top10ranking, indent=4)
 #     jp.write(js)
+
+
+
+
+
+
+
+
+
+
+
+# Para esperar até que uma condição seja satisfeita dado um limite de tempo
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.common.exceptions import TimeoutException
+
+# delay = 3 # seconds
+# try:
+#     myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
+#     print "Page is ready!"
+# except TimeoutException:
+#     print "Loading took too much time!"
