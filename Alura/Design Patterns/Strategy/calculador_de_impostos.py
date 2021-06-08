42d9c32260cc3f7c5c552b5224c1faac42c6202a
Familia <Familia@DESKTOP-SSIR9HJ>
2021-06-08 19:26:59 -0300

    design pattern strategy - concluído

--- Alura/Design Patterns/Strategy/calculador_de_impostos.py
+++ Alura/Design Patterns/Strategy/calculador_de_impostos.py
@@ -2,15 +2,16 @@
 
 class Calculador_de_impostos(object):
 
-  def realiza_calculo(self, orcamento):
+  def realiza_calculo(self, orcamento, imposto):
 
-    icms_calculado = orcamento.valor * 0.1
-    print(icms_calculado)
+      print(imposto(orcamento))
 
 if __name__ == '__main__':
-
-    from .orcamento import Orcamento
+    from orcamento import Orcamento
+    from impostos import calcula_ICMS, calcula_ISS
 
     orcamento = Orcamento(500.0)
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(orcamento) ## imprimirá 50.0
+    # ISS é 6% do orçamento e ICMS é 10% do orçamento
+    calculador_de_impostos.realiza_calculo(orcamento, calcula_ISS) ## exibirá 30
+    calculador_de_impostos.realiza_calculo(orcamento, calcula_ICMS) ## exibirá 50
